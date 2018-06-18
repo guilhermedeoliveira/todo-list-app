becac87a8ed7e0c6b4f14df428ed24fe324d49b1
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2018-06-18 17:13:31 -0300

    init 'Style'

---
+++ client/Style.js
@@ -0,0 +1,46 @@
+import { Dimensions, Platform } from 'react-native';
+
+// Dimensions
+const { height, width } = Dimensions.get('window');
+
+const ratioX = width < 375 ? (width < 320 ? 0.75 : 0.875) : 1; // eslint-disable-line
+const ratioY = height < 568 ? (height < 480 ? 0.75 : 0.875) : 1; // eslint-disable-line
+
+export const em = (tm) => { // eslint-disable-line
+  const unit = 16 * ratioX;
+  return unit * tm;
+};
+
+export const wp = (percentage) => {
+  const value = (percentage * width) / 100;
+  return Math.round(value);
+};
+
+export const isIOS = Platform.OS === 'ios';
+
+// Colors, font, sizes
+export default {
+  colors: {
+    white: '#ffffff',
+    transparent: 'transparent'
+  },
+  button: {
+    borderRadius: 25
+  },
+  fontSize: {
+    tiny: em(0.6),
+    small: em(0.8),
+    regular: em(1),
+    large: em(1.3),
+    extraLarge: em(1.6)
+  },
+  fontWeight: {
+    light: '100',
+    regular: '400',
+    medium: '500',
+    bold: '900'
+  },
+  fontFamily: '',
+  width,
+  height
+};
