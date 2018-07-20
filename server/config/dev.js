ff711270ae2f4b17155d04453f2fa80b93aa7e95
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2018-07-20 11:14:09 -0300

    update some dev keys

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,7 +1,9 @@
 module.exports = {
+  facebookClientID: '245349979601173',
+  facebookClientSecret: '62348743de9d42ad65e61543846b2292',
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://guilhermedeoliveira:insight6@ds227821.mlab.com:27821/todo-list',
   cookieKey: '123123123'
 };
