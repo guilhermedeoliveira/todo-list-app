3bff337be066b0d0223ae8718e1c00474ad5b878
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2018-08-17 10:22:08 -0300

    turn 'task.id' string

--- client/src/modules/home/Home.data.js
+++ client/src/modules/home/Home.data.js
@@ -1,8 +1,8 @@
 export default [
-  { id: 0, text: 'Task 01', isChecked: false },
-  { id: 1, text: 'Task 02', isChecked: true },
-  { id: 2, text: 'Task 03', isChecked: false },
-  { id: 3, text: 'Task 04', isChecked: true },
-  { id: 4, text: 'Task 05', isChecked: false },
-  { id: 5, text: 'Task 06', isChecked: true }
+  { id: '00', text: 'Task 01', isChecked: false },
+  { id: '01', text: 'Task 02', isChecked: true },
+  { id: '02', text: 'Task 03', isChecked: false },
+  { id: '03', text: 'Task 04', isChecked: true },
+  { id: '04', text: 'Task 05', isChecked: false },
+  { id: '05', text: 'Task 06', isChecked: true }
 ];
