230f62d7db18bc0df51ad9d4ad95fccd626e6a2e
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2018-09-17 22:14:20 -0300

    create 'Signup' route

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,15 +1,17 @@
 import { createStackNavigator } from 'react-navigation';
 
 import LoginScreen from './modules/login/LoginScreen';
+import SignupScreen from './modules/signup/SignupScreen';
 import HomeScreen from './modules/home/HomeScreen';
 
 const Router = createStackNavigator(
   {
     Login: LoginScreen,
+    Signup: SignupScreen,
     Home: HomeScreen
   },
   {
-    initialRouteName: 'Home'
+    initialRouteName: 'Login'
   }
 );
 
