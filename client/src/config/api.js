d19c8bea731d54c97322505112d5f7d5b9ab6389
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2018-09-17 22:10:37 -0300

    create config file

---
+++ client/src/config/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_URL = '';
+
+const API = axios.create({
+  baseURL: API_URL
+});
+
+const SIGNIN_ENDPOINT = '/auth/signin';
+const SIGNUP_ENDPOINT = '/auth/signup';
+const TASK_ENDPOINT = '/task';
+
+export default {
+  signin: user => API.post(SIGNIN_ENDPOINT, user),
+  signup: user => API.post(SIGNUP_ENDPOINT, user),
+
+  getAllTasks: () => API.get(TASK_ENDPOINT).then(response => response.data),
+  getTaskById: taskId => API.get(`${TASK_ENDPOINT}/${taskId}`).then(response => response.data)
+};
