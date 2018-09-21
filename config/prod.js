a83b7d245bd15ab8c7b6ce7f0fd73ced9f77fb3d
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2018-09-20 21:43:15 -0300

    set prod keys

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,8 @@
 module.exports = {
-  mongoURI: process.env.mongoURI
+  mongoURI: process.env.mongoURI,
+  clientID: process.env.clientID,
+  authClientSecret: process.env.authClientSecret,
+  cookieKey: process.env.cookieKey,
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret
 };
